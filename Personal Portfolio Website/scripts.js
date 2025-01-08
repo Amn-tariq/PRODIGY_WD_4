8ed3f3abc11b770c358ba2ddc7fc7c10f9a17488
Amn-tariq <142118508+Amn-tariq@users.noreply.github.com>
2025-01-08 16:34:32 +0500

    .

---
+++ Personal Portfolio Website/scripts.js
@@ -0,0 +1,6 @@
+// Handle Contact Form Submission
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+    alert("Thank you for contacting me! I'll get back to you soon.");
+  });
+  
